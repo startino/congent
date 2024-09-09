0cb2403fd1cd1ec4033d53533d7b25ac5c0d1ea7
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-09-09 23:01:47 +0800

    adding supabase buckets stuff for project management to come

--- services/ai-service/querying/knowledge_graph_loader.py
+++ services/ai-service/querying/knowledge_graph_loader.py
@@ -4,7 +4,10 @@ import pandas as pd
 
 class KnowledgeGraphLoader:
     def __init__(self, project_name: str, supabase: Client):
-        self.project_name = project_name
+        # code for when profile and project management is setup on frontend
+        # self._profile_id = profile_id
+        # self._project_id = project_id
+        self._project_name = project_name
         self._supabase = supabase
         self._entity_df = None
         self._entity_embedding_df = None
@@ -13,7 +16,9 @@ class KnowledgeGraphLoader:
         self._text_unit_df = None
 
     def _load_parquet(self, file_path: str):
-        file_bytes = self._supabase.storage.from_('knowledge_graphs').download(file_path)
+        # code for when profile and project management is setup on frontend
+        # file_bytes = self._supabase.storage.from_('knowledge_graphs').download(f"{self._profile_id}/{self._project_id}/file_path")
+        file_bytes = self._supabase.storage.from_('knowledge_graphs').download(f"{self._project_name}/{file_path}")
         if file_bytes is None:
             raise FileNotFoundError(f"File not found: {file_path}")
         file_buffer = BytesIO(file_bytes)
@@ -22,29 +27,29 @@ class KnowledgeGraphLoader:
     @property
     def entity_df(self):
         if self._entity_df is None:
-            self._entity_df = self._load_parquet(f"/{self.project_name}/create_final_nodes.parquet")
+            self._entity_df = self._load_parquet("/create_final_nodes.parquet")
         return self._entity_df
 
     @property
     def entity_embedding_df(self):
         if self._entity_embedding_df is None:
-            self._entity_embedding_df = self._load_parquet(f"/{self.project_name}/create_final_entities.parquet")
+            self._entity_embedding_df = self._load_parquet("/create_final_entities.parquet")
         return self._entity_embedding_df
 
     @property
     def relationship_df(self):
         if self._relationship_df is None:
-            self._relationship_df = self._load_parquet(f"/{self.project_name}/create_final_relationships.parquet")
+            self._relationship_df = self._load_parquet("/create_final_relationships.parquet")
         return self._relationship_df
 
     @property
     def report_df(self):
         if self._report_df is None:
-            self._report_df = self._load_parquet(f"/{self.project_name}/create_final_community_reports.parquet")
+            self._report_df = self._load_parquet("/create_final_community_reports.parquet")
         return self._report_df
 
     @property
     def text_unit_df(self):
         if self._text_unit_df is None:
-            self._text_unit_df = self._load_parquet(f"/{self.project_name}/create_final_text_units.parquet")
+            self._text_unit_df = self._load_parquet("/create_final_text_units.parquet")
         return self._text_unit_df
--- services/ai-service/supabase_download_files_demo.py
+++ services/ai-service/supabase_download_files_demo.py
@@ -14,13 +14,12 @@ key = os.getenv("SUPABASE_ANON_KEY")
 supabase: Client = create_client(url, key)
 
 
-file_bytes = supabase.storage.from_('knowledge_graphs').download(f"/readai_aug_8/create_final_nodes.parquet")
-print(type(file_bytes))
+# file_bytes = supabase.storage.from_('knowledge_graphs').download(f"/readai_aug_8/create_final_nodes.parquet")
+# print(type(file_bytes))
 
-# Convert bytes to BytesIO
-file_buffer = BytesIO(file_bytes)
+# # Convert bytes to BytesIO
+# file_buffer = BytesIO(file_bytes)
 
-entity_df = pd.read_parquet(file_bytes)
+# entity_df = pd.read_parquet(file_bytes)
 
-print(entity_df.head())
-
+# print(entity_df.head())
